97f616191866d5dfff195dec186cf4398f9d0557
Koji Shigemura <shige0501@gmail.com>
2016-04-26 23:28:28 +0900

    バーコードのサンプル追加

--- app/src/main/java/net/buildbox/sample/sample_zxing/MainActivity.java
+++ app/src/main/java/net/buildbox/sample/sample_zxing/MainActivity.java
@@ -1,7 +1,15 @@
 package net.buildbox.sample.sample_zxing;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.barcodeScanButton)
+    public void onBarcodeScanClick() {
+        new IntentIntegrator(this).initiateScan();
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (intentResult != null) {
+            String content = intentResult.getContents();
+            Toast.makeText(this, content, Toast.LENGTH_SHORT).show();
+        }
     }
 }
